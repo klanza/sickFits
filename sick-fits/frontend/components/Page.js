d7993cce98ea6213ae660b18407bef6e0b302223
klanza <klanza91@gmail.com>
2019-01-19 15:27:16 -0700

    Add styling to header and main page

--- sick-fits/frontend/components/Page.js
+++ sick-fits/frontend/components/Page.js
@@ -6,7 +6,7 @@ import styled, { ThemeProvider, injectGlobal } from 'styled-components';
 
 const theme = {
   red: '#FF0000',
-  black: '#3393939',
+  black: '#393939',
   grey: '#3A3A3A',
   lightgrey: '#E1E1E1',
   offwhite: '#EDEDED',
