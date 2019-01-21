9b2a8dc0d72e8c331965de6fa5cc21f606eb2813
klanza <klanza91@gmail.com>
2019-01-20 17:43:30 -0700

    Further backend development, first mutation/allitem query

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,25 @@
-const Mutation = {};
+const Mutation = {
+  async createItem(parent, args, ctx, info) {
+    // TODO: Check if they are logged in
+
+    // ctx.db is how to access db, can also export db into file
+    const item = await ctx.db.mutation.createItem(
+      {
+        data: {
+          ...args
+        }
+      },
+      info
+    );
+    return item;
+  }
+  // createDog(parent, args, ctx, info) {
+  //   global.dogs = global.dogs || [];
+  //   // create a dog
+  //   const newDog = { name: args.name };
+  //   global.dogs.push(newDog);
+  //   return newDog;
+  // }
+};
 
 module.exports = Mutation;
