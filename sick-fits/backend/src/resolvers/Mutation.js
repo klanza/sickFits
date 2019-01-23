5270697b0d6c3a58fa27a8c65cbe536502285989
klanza <klanza91@gmail.com>
2019-01-22 18:51:07 -0700

    Add GraphQL mutation for deleting item

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,25 +1,36 @@
-const Mutation = {
+const mutations = {
   async createItem(parent, args, ctx, info) {
-    // TODO: Check if they are logged in
-
-    // ctx.db is how to access db, can also export db into file
     const item = await ctx.db.mutation.createItem(
       {
         data: {
-          ...args
-        }
+          ...args,
+        },
       },
       info
     );
     return item;
-  }
-  // createDog(parent, args, ctx, info) {
-  //   global.dogs = global.dogs || [];
-  //   // create a dog
-  //   const newDog = { name: args.name };
-  //   global.dogs.push(newDog);
-  //   return newDog;
-  // }
+  },
+  updateItem(parent, args, ctx, info) {
+    // first take a copy of the updates
+    const updates = { ...args };
+    // remove the ID from the updates
+    delete updates.id;
+    // run the update method
+    return ctx.db.mutation.updateItem(
+      {
+        data: updates,
+        where: {
+          id: args.id,
+        },
+      },
+      info
+    );
+  },
+  async deleteItem(parent, args, ctx, info) {
+    const where = { id: args.id };
+    const item = await ctx.db.query.item({ where }, `{id title}`);
+    return ctx.db.mutation.deleteItem({ where }, info);
+  },
 };
 
-module.exports = Mutation;
+module.exports = mutations;
